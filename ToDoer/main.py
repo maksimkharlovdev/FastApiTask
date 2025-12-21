1359f6452f05f97283a3a7e582ee0e69ee5c7528
wayoneget <vova21773@gmail.com>
2025-12-21 21:09:29 +0500

    Initial

--- ToDoer/main.py
+++ ToDoer/main.py
@@ -1,2 +1,29 @@
-if __name__ == "__main__":
-    print("Hello w3orld")
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
+from uuid import uuid4
+from database import Database
+
+app = FastAPI(title="ToDoer", version="1.0")
+db = Database()
+
+@app.get("/")
+async def root():
+    return {
+        "message": "УРААА!",
+        "endpoints": {
+            "GET /items": "Получить все товары"
+        }
+    }
+
+@app.get("/tasks")
+async def get_all_tasks():
+    return db.get()
+
+@app.get("/tasks/{title}")
+async def add_task(title):
+    return db.get(title)
+
+@app.post("/tasks/{title}")
+async def get_items(title):
+    return db.add(title)
